8f10d5dea58d4ce61793cf7befb1f033898b3613
shrutirthr29 <shray.shruti@gmail.com>
2024-02-20 23:27:28 +0530

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,19 +115,59 @@
 
 "use client";
 import React from "react";
-// import { WavyBackground } from "../ui/wavy-background";
 import { WavyBackground } from "../components/ui/wavy-background";
+import { FloatingNav } from "../components/ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 
-export default function WavyBackgroundDemo() {
+export default function Home() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
+  const DummyContent = () => {
+    return (
+      <div className="grid grid-cols-1 h-[40rem] w-full bg-white dark:bg-black relative border border-neutral-200 dark:border-white/[0.2] rounded-md">
+        <p className="dark:text-white text-neutral-600 text-center text-4xl mt-40 font-bold">
+          Scroll back up to reveal Navbar
+        </p>
+        <div className="inset-0 absolute bg-grid-black/[0.1] dark:bg-grid-white/[0.2]" />
+      </div>
+    );
+  };
   return (
-    <WavyBackground className="max-w-4xl mx-auto pb-40">
-      <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-        ELIXIR
-      </p>
-      <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
-        The Tech Community
-      </p>
-    </WavyBackground>
+    <>
+      <WavyBackground className="max-w-4xl mx-auto pb-40">
+        <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+          ELIXIR
+        </p>
+        <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+          The Tech Community
+        </p>
+      </WavyBackground>
+      <div className="relative  w-full">
+        <FloatingNav navItems={navItems} />
+        <DummyContent />
+      </div>
+    </>
+
+
   );
 }
+
+
 
