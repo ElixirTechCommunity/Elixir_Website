a90bfbc7cec33f56841519c8a0983c5c1315a527
arjunomray <arjunomray@gmail.com>
2024-02-22 23:46:36 +0530

    RELEASE 0.0.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com', 'images.unsplash.com'],
+  },
+};
 
 export default nextConfig;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { cn } from '@/utils/cn';
+import React, { useState } from 'react';
+import { HoveredLink, Menu, MenuItem } from './ui/navbar-menu';
+import Link from 'next/link';
+
+export function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+
+  return (
+    <div
+      className={cn('fixed top-10 inset-x-0 max-w-2xl mx-auto z-50', className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href="/">
+          <MenuItem setActive={setActive} active={active} item="Home">
+            {/* <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/">Home</HoveredLink>
+          </div> */}
+          </MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">
+              Basic Music Theory
+            </HoveredLink>
+            <HoveredLink href="/courses">
+              Advanced Composition
+            </HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">
+              Music Production
+            </HoveredLink>
+          </div>
+        </MenuItem>
+
+        <Link href="/contact">
+          <MenuItem setActive={setActive} active={active} item="Contact Us">
+            {/* <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/contact">Contact Us</HoveredLink>
+          </div> */}
+          </MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+}
