4d452f11f1526d68d53f3acd90b2765a40569617
Vandit Singh <vanditsinghkv@gmail.com>
2024-02-23 23:42:29 +0530

    add routes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 // Import necessary types and utilities
 import type { Config } from 'tailwindcss';
 
+const svgToDataUri = require('mini-svg-data-uri');
+
 const {
   default: flattenColorPalette,
 } = require('tailwindcss/lib/util/flattenColorPalette');
-
-const svgToDataUri = require('mini-svg-data-uri');
 
 // Plugin to add each Tailwind color as a global CSS variable
 function addVariablesForColors({ addBase, theme }: any) {
@@ -58,7 +58,7 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       animation: {
-         "meteor-effect": "meteor 5s linear infinite",
+        "meteor-effect": "meteor 5s linear infinite",
         spotlight: 'spotlight 2s ease .75s 1 forwards',
         scroll:
           'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
@@ -122,7 +122,33 @@ const config: Config = {
       },
     },
   },
-  plugins: [addVariablesForColors, addSvgPatterns],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    addSvgPatterns,
+    addVariablesForColors,
+    function({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
 
 export default config;
