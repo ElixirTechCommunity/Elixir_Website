f1e016371da9141c117bac05ece263e44f2bb683
Vandit Singh <vanditsinghkv@gmail.com>
2024-02-23 23:53:38 +0530

    add dotted background

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,19 +8,25 @@ const words = `Explore. Learn. Conquer`;
 export function HeroSection() {
   return (
     <div className="flex flex-col overflow-hidden">
-      <ContainerScroll
-        users={users}
-        titleComponent={
-          <>
-            <h1 className="text-xl font-semibold text-black dark:text-white">
-          <TextGenerateEffect words={words} />    <br />
-              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                Elixir Tech Community
-              </span>
-            </h1>
-          </>
-        }
-      />
+      <div className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+          <ContainerScroll
+            users={users}
+            titleComponent={
+              <>
+                <h1 className="text-xl font-semibold text-black dark:text-white">
+                  <TextGenerateEffect words={words} />    <br />
+                  <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                    Elixir Tech Community
+                  </span>
+                </h1>
+              </>
+            }
+          />
+        </p>
+      </div>
     </div>
   );
 }
