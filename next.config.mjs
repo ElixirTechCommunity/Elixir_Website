dabcd66cd928c4c45d7aa996c1ec73714c6bc188
Vandit Singh <vanditsinghkv@gmail.com>
2024-02-23 15:44:58 +0530

    add ui acertinity as image source

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['ui.aceternity.comm','res.cloudinary.com', 'images.unsplash.com'],
+    domains: ['ui.aceternity.com','res.cloudinary.com', 'images.unsplash.com'],
   },
 };
 
