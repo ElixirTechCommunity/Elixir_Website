f8b36c838e97898cb69a0c5850f34d2d57a4a8f6
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-02-24 14:20:46 +0530

    populate event cards through API requests

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['ui.aceternity.com','res.cloudinary.com', 'images.unsplash.com', 'via.placeholder.com'],
+    domains: ['ui.aceternity.com','res.cloudinary.com', 'images.unsplash.com', 'via.placeholder.com', 'i.ibb.co'],
   },
 };
 
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import EventsCard from "@/components/EventsCard3D";
 
 export default function Events(): JSX.Element {
+    const [events, setEvents] = useState([]);
+    const fetchevents = async () => {
+        try {
+            const events = await axios.get(
+                "https://elixir-backendv2.vercel.app/events/?format=json"
+            );
+            setEvents(events.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchevents();
+    }, []);
+
+    console.log(events);
+    
     return (
-        <main className="relative bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-            <div className="flex flex-wrap justify-center gap-4 pt-52 pb-24 max-w-[70dvw] mx-auto">
-                <EventsCard
-                    title="Event Title"
-                    description="Event Description"
-                    eventName="Event Name"
-                    eventImage="https://via.placeholder.com/1000"
-                    clubName="Club Name"
-                    clubImage="https://via.placeholder.com/25"
-                    deadline="2022-12-31T23:59:59Z"
-                    isOver={false}
-                />
+        <main className="min-h-screen relative antialiased bg-grid-slate-100 dark:bg-grid-white/[0.02]">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center items-stretch gap-10 pt-52 pb-24 max-w-[70dvw] mx-auto">
+                {events.map((event: any) => (
+                    <EventsCard
+                        key={event.name}
+                        title={event.name}
+                        description={event.event_summary}
+                        eventName={event.name}
+                        eventImage={event.img_link}
+                        clubName={event.club}
+                        clubImage="https://via.placeholder.com/25"
+                        deadline={event.date}
+                        formLink={event.form_link}
+                        isOver={false}
+                    />
+                ))}
             </div>
         </main>
     );
