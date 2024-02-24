3a57dcd4457f610b45de68a7faff73fbe445de35
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-02-24 14:16:50 +0530

    remove redundant background styling from hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,9 +8,9 @@ const words = `Explore. Learn. Conquer`;
 export function HeroSection() {
   return (
     <div className="flex flex-col overflow-hidden">
-      <div className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+      <div className="w-full relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
           <ContainerScroll
             users={users}
