f49748b5b0231cf37a9a303c254eaf99d165191e
Somdutta chakraborty <somdutta26403@gmail.com>
2024-02-25 19:18:44 +0530

    update footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,33 +2,38 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer className="bg-cover bg-center py-16" style={{ backgroundImage: "url('../../public/footer_rays.png')" }}>
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className="col-span-1">
-            <h2 className="dark:text-white light:text-black text-lg font-semibold mb-4">About Us</h2>
-            <p className="dark:text-white light:text-black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non feugiat elit. Cras in ex ut elit hendrerit efficitur.</p>
-          </div>
-          <div className="col-span-1">
-            <h2 className="dark:text-white light:text-black text-lg font-semibold mb-4">Contact Us</h2>
-            <p className="dark:text-white light:text-black">add gmail</p>
-          </div>
-          <div className="col-span-1">
-            <h2 className="dark:text-white light:text-black text-lg font-semibold mb-4">Follow Us</h2>
-            <div className="flex items-center space-x-4">
-              <a href="#" className="text-gray-400 hover:text-indigo-600 dark:text-white light:text-black">
-                 Twitter
-              </a>
-              <a href="#" className="text-gray-400 hover:text-indigo-600 dark:text-white light:text-black">
-                Facebook
-              </a>
-              <a href="#" className="text-gray-400 hover:text-indigo-600 dark:text-white light:text-black">
-                Instagram
-              </a>
-            </div>
+    <footer className="bg-black text-gray-400 py-12">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
+          <div className="flex space-x-4">
+            <a
+              href="https://www.linkedin.com/company/elixir-tech-community/"
+              className="hover:text-white transition-colors duration-300"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://twitter.com/TheElixirTech"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Twitter
+            </a>
           </div>
         </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
+          <p className="mb-4">
+          Elixir: A student community uniting ABESEC's GeeksforGeeks, GDSC, and Codechef chapters, fostering student growth and networking.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
+          <p>Email: info@musicschool.com</p>
+          
+        </div>
       </div>
+    
     </footer>
   );
 };
