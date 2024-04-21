2a3b6ea9d457d1848a80c669c3b7f1c4746b3794
dewank07 <dewankrastogi007@gmail.com>
2024-04-21 20:32:50 +0530

    new landing page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 "use client";
 import React from "react";
-import HeroSection from "@/components/HeroSection";
-import ClubsInformation from "@/components/ClubsInformation";
-import { JoinUs } from "@/components/JoinUs";
-import { ElixirTabs } from "@/components/ElixirTabs";
+// import HeroSection from "@/components/HeroSection";
+// import ClubsInformation from "@/components/ClubsInformation";
+// import { JoinUs } from "@/components/JoinUs";
+// import { ElixirTabs } from "@/components/ElixirTabs";
 import useEventApi from "@/hooks/useEventApi";
+import Header from "@/components/landingPage/Header";
+import Hero from "@/components/landingPage/Hero";
+import Benefits from "@/components/landingPage/Benefits";
+import Collaboration from "@/components/landingPage/Collaboration";
+import Services from "@/components/landingPage/Services";
+import Pricing from "@/components/landingPage/Pricing";
+import Roadmap from "@/components/landingPage/Roadmap";
+import Footer from "@/components/landingPage/Footer";
 
 export default function Home() {
   useEventApi();
   return (
-    <main>
-      <HeroSection />
-      <ClubsInformation />
-      <ElixirTabs />
-      <JoinUs />
-    </main>
+    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+      <Header />
+      <Hero />
+      <Benefits />
+      <Collaboration />
+      <Services />
+      <Pricing />
+      <Roadmap />
+      <Footer />
+    </div>
   );
 }
