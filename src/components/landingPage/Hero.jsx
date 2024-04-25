afd089a5aeb18d19bd0118b6a5123b0067c68d35
Rudraksh Tyagi <rudraksh875@gmail.com>
2024-04-26 03:52:42 +0530

    chore: replace participate form states with refs
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- src/components/landingPage/Hero.jsx
+++ src/components/landingPage/Hero.jsx
@@ -91,7 +91,6 @@ const Hero = () => {
           <BackgroundCircles />
         </div>
 
-        <CompanyLogos className='hidden relative z-10 mt-20 lg:block' />
       </div>
 
       <BottomLine />
