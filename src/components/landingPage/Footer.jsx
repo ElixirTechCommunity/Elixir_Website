802d7c0a1adddd406312ceeb44c99bd6ad272942
Vandit Singh <107131545+Vandit1604@users.noreply.github.com>
2024-04-28 00:19:35 +0530

    Update Footer.jsx

--- src/components/landingPage/Footer.jsx
+++ src/components/landingPage/Footer.jsx
@@ -7,10 +7,6 @@ const Footer = () => {
   return (
     <Section crosses className='!px-0 !py-10'>
       <div className='container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col'>
-        {/* <p className='caption text-n-4 lg:block'>
-          © {new Date().getFullYear()}. All rights reserved.
-        </p> */}
-
         <ul className='flex gap-20 flex-wrap '>
           {socials.map((item) => (
             <a
